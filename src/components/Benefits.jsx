2390bca38cdee0c49175df6ab9fb97f3f3da9f30
Rainer Nsa <woldreamz@gmail.com>
2024-07-28 17:45:29 +0100

    City of God

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -11,7 +11,7 @@ const Benefits = () => {
       <div className="container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
-          title="Festival Gallery"
+          title="The Ultimate Festival Experience"
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
--- src/constants/index.js
+++ src/constants/index.js
@@ -75,7 +75,7 @@ export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
 
 export const notificationImages = [notification4, notification3, notification2];
 
-export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+export const companyLogos = [yourlogo];
 
 export const brainwaveServices = [
   "Photo generating",
@@ -244,16 +244,16 @@ export const pricing = [
 export const benefits = [
   {
     id: "0",
-    title: "Ask Away",
-    text: "Lets users to swiftly find answers to their questions without sifting through multiple sources.",
+    title: "Live Music",
+    text: "Experience electrifying performances from top artists and bands.",
     backgroundUrl: "assets/benefits/card-1.svg",
     iconUrl: benefitIcon1,
     imageUrl: benefitImage2,
   },
   {
     id: "1",
-    title: "Improve everyday",
-    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+    title: "Art and Culture",
+    text: "Explore captivating art installations and cultural shows.",
     backgroundUrl: "assets/benefits/card-2.svg",
     iconUrl: benefitIcon2,
     imageUrl: benefitImage2,
@@ -261,16 +261,16 @@ export const benefits = [
   },
   {
     id: "2",
-    title: "Connect everywhere",
-    text: "Connect with the AI chatbot from anywhere, on any device, making it more accessible and convenient.",
+    title: "Activities and Events",
+    text: "Get details on scheduled performances, workshops, and more.",
     backgroundUrl: "assets/benefits/card-3.svg",
     iconUrl: benefitIcon3,
     imageUrl: benefitImage2,
   },
   {
     id: "3",
-    title: "Fast responding",
-    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+    title: "Food and Drink",
+    text: "Savor a diverse range of culinary delights and refreshing beverages.",
     backgroundUrl: "assets/benefits/card-4.svg",
     iconUrl: benefitIcon4,
     imageUrl: benefitImage2,
@@ -278,16 +278,16 @@ export const benefits = [
   },
   {
     id: "4",
-    title: "Ask Away",
-    text: "Lets users to swiftly find answers to their questions without sifting through multiple sources.",
+    title: "Family Fun",
+    text: "Enjoy activities and workshops designed for families and kids.",
     backgroundUrl: "assets/benefits/card-5.svg",
     iconUrl: benefitIcon1,
     imageUrl: benefitImage2,
   },
   {
     id: "5",
-    title: "Improve everyday",
-    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+    title: "Crowd Moments",
+    text: "Witness the joy and excitement of our festival-goers.",
     backgroundUrl: "assets/benefits/card-6.svg",
     iconUrl: benefitIcon2,
     imageUrl: benefitImage2,
